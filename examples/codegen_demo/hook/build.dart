9353f18c1b83985d82f6e93b8aac5251153e1fb1
deretame <deretame@outlook.com>
2026-07-27 17:11:00 +0800

    ci: optimize workflow + add Android emulator test

--- examples/codegen_demo/hook/build.dart
+++ examples/codegen_demo/hook/build.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:code_assets/code_assets.dart';
 import 'package:dart_cpp_bridge/hook.dart';
 import 'package:hooks/hooks.dart';
@@ -7,14 +9,23 @@ void main(List<String> args) async {
     if (!input.config.buildCodeAssets) {
       return;
     }
+    final cmake = Platform.environment['NIX_DCB_CMAKE'] ??
+        r'C:\Program Files\Microsoft Visual Studio\18\Community'
+            r'\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe';
     final config = switch (input.config.code.targetOS) {
-      OS.windows => const WindowsConfig(),
-      OS.linux => const LinuxConfig(),
-      OS.android => const AndroidConfig(
-          cmake:
-              r'C:\Program Files\Microsoft Visual Studio\18\Community\Common7\IDE\CommonExtensions\Microsoft\CMake\CMake\bin\cmake.exe',
-          ndkPath:
+      OS.windows => WindowsConfig(cmake: cmake),
+      OS.linux => LinuxConfig(cmake: cmake),
+      OS.android => AndroidConfig(
+          cmake: cmake,
+          ndkPath: Platform.environment['ANDROID_NDK_HOME'] ??
               r'C:\Users\windy\AppData\Local\Android\Sdk\ndk\29.0.14206865',
+          abi: switch (input.config.code.targetArchitecture) {
+            Architecture.arm64 => 'arm64-v8a',
+            Architecture.arm => 'armeabi-v7a',
+            Architecture.x64 => 'x86_64',
+            Architecture.ia32 => 'x86',
+            final a => throw UnsupportedError('Unsupported Android arch: $a'),
+          },
         ),
       final os => throw UnsupportedError(
           'codegen_demo does not support: $os'),
